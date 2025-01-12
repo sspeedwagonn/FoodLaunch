b0169e5fb5a93eb98b8ee58f32bae38e0a0ae907
sspeedwagonn <burnerforcarter@gmail.com>
2025-01-12 15:38:26 -0600

    Added IF and created config inv

--- src/main/java/net/civicraft/foodLaunch/gui/ConfigGUI.java
+++ src/main/java/net/civicraft/foodLaunch/gui/ConfigGUI.java
@@ -1,9 +1,12 @@
 package net.civicraft.foodLaunch.gui;
 
+import com.github.stefvanschie.inventoryframework.gui.type.ChestGui;
 import org.bukkit.entity.Player;
 
 public class ConfigGUI {
     public static void cfg(Player player) {
+        ChestGui cfg = new ChestGui(6, "FoodLaunch Config");
 
+        cfg.show(player);
     }
 }
